7e2396e2599c336c4ce5c41c8d7d552c206f1963
JoseanSanz <alumno@eim-vir-80120.lab.unavarra.es>
2025-11-17 12:46:32 +0100

    GUI in english

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import gradio as gr
 import requests
 
-# URL de tu API FastAPI
-API_URL = "https://my-fastapi-main-latest.onrender.com"
+# URL of the API created with FastAPI
+# API_URL = "https://my-fastapi-main-latest.onrender.com"
+API_URL = "https://calculator-latest.onrender.com"
 
 
-# Función que se ejecuta al presionar "Calcular"
+# Function to execute when clicking the "Compute button"
 def calcular(a, b, operacion):
     try:
-        payload = {"a": float(a), "b": float(b), "operation": operacion}
+        payload = {"a": float(a), "b": float(b), "op": operacion}
         response = requests.post(f"{API_URL}/calculate", json=payload, timeout=5)
         response.raise_for_status()
         data = response.json()
@@ -17,23 +18,23 @@ def calcular(a, b, operacion):
         return f"Error: {response.json().get('detail', str(e))}"
 
 
-# Crear interfaz Gradio
+# GUI creted using Gradio
 iface = gr.Interface(
     fn=calcular,
     inputs=[
-        gr.Number(label="Número X"),
-        gr.Number(label="Número Y"),
+        gr.Number(label="Number X"),
+        gr.Number(label="Number Y"),
         gr.Dropdown(
             choices=["add", "subtract", "multiply", "divide", "power"],
             value="add",
-            label="Operación",
+            label="Arithmetical operation",
         ),
     ],
-    outputs=gr.Textbox(label="Resultado"),
-    title="Calculadora con FastAPI y Gradio",
-    description="Calculadora interactiva que llama a la API FastAPI /calculate",
+    outputs=gr.Textbox(label="Result"),
+    title="Calculator created with FastAPI and Gradio",
+    description="Interactive calculator using the endpoint /calculate",
 )
 
-# Lanzar interfaz
+# Launch the GUI
 if __name__ == "__main__":
     iface.launch()
